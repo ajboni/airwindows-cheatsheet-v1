d0de88089f9c608c882d6835eda3270699f8f7c2
Tiki Horea <tiki2134@gmail.com>
2020-06-06 23:42:13 +0300

    fixed some typos in readme.md and header.svelte

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Cheatsheet for airwindows plugins.
 
 # How to contribute.
 
-If you know your git, just do a PR. The database of plugins is stored at `database.js`.
+If you know your git, just do a PR. The database of plugins is stored in `database.js`.
 Otherwise please [fill out an issue](https://github.com/ajboni/airwindows-cheatsheet/issues/new)
 
 # License
--- src/Header.svelte
+++ src/Header.svelte
@@ -17,7 +17,7 @@
       </h2>
       <p>
         This site is meant to be a quick reference for the constantly growing list
-        of <a href="https://www.airwindows.com">AirWindows Plugins.</a>
+        of <a href="https://www.airwindows.com">Airwindows Plugins.</a>
         <br />
         Info originally collected by
         <a href="https://tikihorea.com" target="_blank">Tiki Horea</a>
