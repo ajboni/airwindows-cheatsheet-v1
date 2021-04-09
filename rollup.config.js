5cc28e3e349fb172f94d7de554fd30d5786ad2e9
ajboni <mail@aboni.dev>
2021-04-09 18:43:28 -0300

    change css for rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 const smelte = require("smelte/rollup-plugin-smelte");
-
+import css from "rollup-plugin-css-only";
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
@@ -21,11 +21,9 @@ export default {
       dev: !production,
       // we'll extract any component CSS out into
       // a separate file - better for performance
-      css: (css) => {
-        css.write("public/build/bundle.css");
-      },
     }),
 
+    css({ output: "bundle.css" }),
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
     // some cases you'll need additional configuration -
