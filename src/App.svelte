8a9ac1fa9aecb489282d95725ba510cbb564b431
Dungtran713 <trandung713@gmail.com>
2021-09-24 16:52:29 -0700

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,10 @@
   import Header from "./Header.svelte";
   import Nav from "./Nav.svelte";
   import Table from "./Table.svelte";
+  import Button from './Toggle.svelte'
+
 </script>
+
 
 <style>
   :global(body) {
@@ -11,9 +14,25 @@
     font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto",
       "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
       "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+      
   }
+  :global(body) {
+		background-color: #f2eee2;
+		color: #0084f6;
+		transition: background-color 0.3s
+	}
+	:global(body.dark-mode) {
+		background-color: #000000
+;
+		color: #bfc2c7;
+	}
+
 </style>
 
 <Nav />
+<Button>
+	Dark Theme
+</Button>
 <Header />
+
 <Table />
